45bd02b6e2677ceb6b6773ebe0f045f0bfa3edfa
Andrey Kuzmenko <andrey.k.qwerty@gmail.com>
2019-07-23 22:20:23 +0300

    change SQL data

--- src/main/resources/db/dataDB.sql
+++ src/main/resources/db/dataDB.sql
@@ -1,12 +1,19 @@
 DELETE FROM USER_ROLES;
 DELETE FROM USERS;
+DELETE FROM RESTORANS;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
   ('User', 'user@yandex.ru', 'password'),
-  ('Admin', 'admin@gmail.com', 'admin');
+  ('Admin', 'admin@gmail.com', 'admin'),
+  ('Manager', 'Manager@yandex.ru', 'manager');
 
 INSERT INTO user_roles (role, user_id) VALUES
-  ('ROLE_USER', 100000),
-  ('ROLE_ADMIN', 100001),
-  ('ROLE_USER', 100001);
+  ('ROLE_USER', (select U.ID FROM USERS U WHERE U.NAME = 'User')),
+  ('ROLE_ADMIN', (select U.ID FROM USERS U WHERE U.NAME = 'Admin')),
+  ('ROLE_ADMIN', (select U.ID FROM USERS U WHERE U.NAME = 'Manager') ),
+  ('ROLE_USER', (select U.ID FROM USERS U WHERE U.NAME = 'Admin'));
+--
+INSERT INTO RESTORANS (NAME, USER_ID)  VALUES
+  ('Star',(select U.ID FROM USERS U WHERE U.NAME = 'Manager') ),
+  ('Pearl',(select  U.ID FROM USERS U WHERE U.NAME = 'Admin'));
